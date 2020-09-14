c4818941aeb16ada5251890af5012eae6e2fb1e5
Kenny1911 <o-muzyka@mail.ru>
2020-09-14 16:27:51 +0400

    refactor: Extract ClassMetadataFactory::getClass() to ClassResolverTrait

--- Mapping/Factory/ClassMetadataFactory.php
+++ Mapping/Factory/ClassMetadataFactory.php
@@ -4,15 +4,13 @@ declare(strict_types=1);
 
 namespace GetInfoTeam\SerializerExtraBundle\Mapping\Factory;
 
-use GetInfoTeam\SerializerExtraBundle\Exception\InvalidArgumentException;
 use GetInfoTeam\SerializerExtraBundle\Mapping\ClassMetadataInterface;
 use GetInfoTeam\SerializerExtraBundle\Mapping\Loader\LoaderInterface;
-use function get_class;
-use function is_object;
-use function is_string;
 
 class ClassMetadataFactory implements ClassMetadataFactoryInterface
 {
+    use ClassResolverTrait;
+
     /** @var LoaderInterface */
     private $loader;
 
@@ -33,26 +31,5 @@ class ClassMetadataFactory implements ClassMetadataFactoryInterface
         }
 
         return $this->loadedClasses[$class];
-    }
-
-    /**
-     * @param string|object $value
-     * @return string
-     */
-    protected function getClass($value): string
-    {
-        if (is_string($value)) {
-            if (!class_exists($value) && !interface_exists($value, false)) {
-                throw new InvalidArgumentException(sprintf('The class or interface "%s" does not exist.', $value));
-            }
-
-            return ltrim($value, '\\');
-        }
-
-        if (!is_object($value)) {
-            throw new InvalidArgumentException(sprintf('Cannot create metadata for non-objects. Got: "%s".', get_debug_type($value)));
-        }
-
-        return get_class($value);
     }
 }
