49b079e8f0511f7277ea5c2dabd6c8bb7346f267
Kenny1911 <o-muzyka@mail.ru>
2020-09-15 14:38:42 +0400

    refactor: Add "$value" parameter to method of ConverterInterface

--- Converter/ConverterInterface.php
+++ Converter/ConverterInterface.php
@@ -11,16 +11,18 @@ interface ConverterInterface
     /**
      * @param object $object
      * @param string $attribute
+     * @param mixed $value
      * @param array $options
      * @return mixed
      */
-    public function convert($object, string $attribute, array $options);
+    public function convert($object, string $attribute, $value, array $options);
 
     /**
      * @param object $object
      * @param string $attribute
+     * @param mixed $value
      * @param array $options
      * @return mixed
      */
-    public function reconvert($object, string $attribute, array $options);
+    public function reconvert($object, string $attribute, $value, array $options);
 }
